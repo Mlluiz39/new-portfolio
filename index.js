87a55e33d267079c4e24feb6303cc3dd7a49fd25
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-02-24 13:47:37 -0300

    reload js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let reload = setTimeout(() => {
   document.location.reload(true)
-}, 500)
+},relaoad1)
 
-clearTimeout(reload)
+const relaoad1 = setInterval(() => {
+ return document.location.reload(false)
+}, 500)
