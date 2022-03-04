a43772e74ddc0da83eebff59b130c43ccb350085
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-04 13:34:13 -0300

    add to transition em experience

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ const moneyValue = document.querySelector('.money-value')
 const mobilegalValue = document.querySelector('.mobilegal-value')
 
 moneyValue.addEventListener('click', () => {
-  const moneyData = document.querySelector('.money-data')
+ const money = document.querySelector('.money-data')
   const borderColor = document.querySelector('.money-value h2')
-  moneyData.classList.toggle('money-data-active')
   borderColor.classList.toggle('border-color-blue')
+  money.classList.toggle('transform-active')
+  
 })
 
 embrasilValue.addEventListener('click', () => {
-  const embraData = document.querySelector('.embra-data')
+  const embrasil = document.querySelector('.embra-data')
   const borderColor = document.querySelector('.embrasil-value h2')
-  embraData.classList.toggle('.embra-data-active')
   borderColor.classList.toggle('border-color-blue')
+  embrasil.classList.toggle('transform-active')
 })
 
 mobilegalValue.addEventListener('click', () => {
-  const mobiData = document.querySelector('.mobi-data')
+  const mobilegal = document.querySelector('.mobi-data')
   const borderColor = document.querySelector('.mobilegal-value h2')
-  mobiData.classList.toggle('mobi-data-active')
   borderColor.classList.toggle('border-color-blue')
+  mobilegal.classList.toggle('transform-active')
 })
