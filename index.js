f86f66c860a61d83cbafea7e4b84757a219ad4c4
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-06 09:45:35 -0300

    add to functions in experiences

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const embrasilValue = document.querySelector('.embrasil-value')
 const moneyValue = document.querySelector('.money-value')
 const mobilegalValue = document.querySelector('.mobilegal-value')
 
-moneyValue.addEventListener('click', () => {
+const moneyFunction = () => {
   const money = document.querySelector('.money-data')
   const borderColor = document.querySelector('.money-value h2')
   borderColor.classList.toggle('border-color-blue')
   money.classList.toggle('transform-active')
-})
+}
 
-embrasilValue.addEventListener('click', () => {
+const embraFunction = () => {
   const embrasil = document.querySelector('.embra-data')
   const borderColor = document.querySelector('.embrasil-value h2')
   borderColor.classList.toggle('border-color-blue')
   embrasil.classList.toggle('transform-active')
-})
+}
 
-mobilegalValue.addEventListener('click', () => {
+const mobiFunction = () => {
   const mobilegal = document.querySelector('.mobi-data')
   const borderColor = document.querySelector('.mobilegal-value h2')
   borderColor.classList.toggle('border-color-blue')
   mobilegal.classList.toggle('transform-active')
-})
+}
+
+moneyValue.addEventListener('click', moneyFunction)
+embrasilValue.addEventListener('click', embraFunction)
+mobilegalValue.addEventListener('click', mobiFunction)
