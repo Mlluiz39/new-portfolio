52f3099dc3877501e08d115ddf8063fb9a61eede
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-03-06 09:41:42 -0300

    add to script js in scroll

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const embrasilValue = document.querySelector('.embrasil-value')
 const moneyValue = document.querySelector('.money-value')
 const mobilegalValue = document.querySelector('.mobilegal-value')
 
-
-
 moneyValue.addEventListener('click', () => {
- const money = document.querySelector('.money-data')
+  const money = document.querySelector('.money-data')
   const borderColor = document.querySelector('.money-value h2')
   borderColor.classList.toggle('border-color-blue')
   money.classList.toggle('transform-active')
-  
 })
 
 embrasilValue.addEventListener('click', () => {
